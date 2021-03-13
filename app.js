1e08034c332a95b08085f929514bd8b69028d0a8
Bulbul <78316289+boolboolean@users.noreply.github.com>
2021-03-13 18:00:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const {fstat} = require("fs");
 var http = require("http")
-var fs = require("fs")
+var fileSystem = require("fs")
 function serveStaticFile(res, path, contentType, responseCode) {
-    if (!responseCode) responseCode = 200;
-    fs.readFile(__dirname + path, function (err, data) {
+    if (!responseCode) 
+    responseCode = 200;
+    fileSystem.readFile(__dirname + path, function (err, data) {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/plain" })
-            res.end("Internal error - 500")
+            res.end("error.html")
           
            
         }
@@ -25,24 +25,23 @@ http.createServer(function (req, res) {
         case "/about":
             serveStaticFile(res, "/about.html", "text/html");
             break;
-
+        case "/video":
+            serveStaticFile(res, "/video/students/memes.mp4", "video/mp4");
+            break;
+        case "/mt2001after3years":
+            serveStaticFile(res, "/img/gallery/graduation.jpg", "image/jpeg");
+            break;
+        case "/mt2001currently":
+            serveStaticFile(res, "/img/gallery/study.jpg", "image/jpeg");
+            break;
         case "/img/about.jpg":
             serveStaticFile(res, "/img/about.jpg", "image/jpeg");
             break;
-        case "/video/students/memes.mp4":
-            serveStaticFile(res, "/video/students/memes.mp4", "video/mp4");
-            break;
-        case "/img/gallery/graduation.jpg":
-            serveStaticFile(res, "/img/gallery/graduation.jpg", "image/jpeg");
-            break;
-        case "/img/gallery/study.jpg":
-                serveStaticFile(res, "/img/gallery/study.jpg", "image/jpeg");
-                break;
         case "/style.css":
             serveStaticFile(res, "/style.css", "text/css");
             break;
         default:
-            serveStaticFile(res, "error.html", "text/html" );
+            serveStaticFile(res, "/error.html", "text/html" );
             break;
     }
 
